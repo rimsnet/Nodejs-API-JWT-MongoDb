bc6b4d9d25f5d9e5e26832b6c9a01b7f3379e5d4
Rimsan <rimsnet@gmail.com>
2021-04-28 11:59:18 +0800

    api with mongodb

---
+++ index.js
@@ -0,0 +1,23 @@
+const express  = require('express');
+const mongoose = require('mongoose');
+const app     = express();
+const dotenv  = require('dotenv');
+
+//Impoort Routes
+const authRoute = require('./routes/auth')
+
+dotenv.config();
+
+//connect to DB
+mongoose.connect(process.env.DB_CONNECT,{ useUnifiedTopology: true, useNewUrlParser: true  }, ()=>{
+    console.log('connected to DB')
+});
+
+//Middleware
+app.use(express.json());
+
+//Route Middleware
+app.use('/api/user', authRoute);
+
+
+app.listen(3000,()=>console.log('Server Up and Running'));
---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const User   = require('../models/User');
+
+router.post('/register', async (req,res)=>{
+  
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password:req.body.password
+    });
+
+    try{
+        const saveUser = await user.save();
+        res.send(saveUser);
+    }catch(err){
+        res.status(400).send(err)
+    }
+
+});
+
+
+
+module.exports = router;
